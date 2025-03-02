91bb7c244636e886c0fd0c35f5daa3e3d69feb82
Luke Thompson <luke.thompson@nine.com.au>
2025-03-03 09:32:36 +1100

    new section

--- src/components/blocks/SectionThree/SectionThree.tsx
+++ src/components/blocks/SectionThree/SectionThree.tsx
@@ -1,9 +1,54 @@
-const SectionThree: React.FC = () => (
-  <section className="section-three relative flex h-screen flex-col items-center justify-center bg-green-500">
-    <h2 className="text-center text-[2rem] leading-normal text-white lg:text-[5.4rem]">
-      headline
-    </h2>
-  </section>
-);
+import { useRef } from 'react';
+import { Layer } from '../../../types';
+import ParallaxBackground from '../../effects/ParallaxBackground';
+import image1 from '../../../assets/images/common/experience-1.png';
+import image2 from '../../../assets/images/common/experience-2.png';
+import image3 from '../../../assets/images/common/experience-3.png';
+import image4 from '../../../assets/images/common/experience-4.png';
+import image5 from '../../../assets/images/common/experience-5.png';
+
+const SectionThree: React.FC = () => {
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const Layers: Layer[] = [
+    {
+      image: image1,
+      depth: 0.1,
+    },
+    {
+      image: image2,
+      depth: 0.2,
+    },
+    {
+      image: image3,
+      depth: 0.3,
+    },
+    {
+      image: image4,
+      depth: 0.4,
+    },
+    {
+      image: image5,
+      depth: 0.5,
+    },
+  ];
+
+  return (
+    <section className="Section-three relative" ref={sectionRef}>
+      {/* Block One */}
+      <div className="block-one relative h-screen w-screen overflow-hidden">
+        <ParallaxBackground
+          layers={Layers}
+          depthOfField={1.5}
+          containerRef={sectionRef}
+        />
+        <div className="absolute bottom-0 h-[15vh] w-full bg-gradient-to-t from-[#101208] to-transparent" />
+      </div>
+
+      {/* Block Two */}
+      <div className="block-two relative h-screen bg-[#101208]" />
+    </section>
+  );
+};
 
 export default SectionThree;
